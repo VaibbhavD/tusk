f470aa6b342c8d86057dda8a5b1ca3ac4c2ce8f2
VaibhavDhamanage <vaibhavdhamanage>
2023-11-09 12:18:14 +0530

    item 2 change back Color green and item 3 invisibal with queryselector using

--- index.js
+++ index.js
@@ -8,9 +8,15 @@
 //    items[3].style.fontWeight='bold';
 //    items[0].style.fontWeight='bold';
 
-var li=document.getElementsByTagName('li');
-li[1].style.backgroundColor='green';
- for(var i=0;i<li.length;i++)
- {
-   li[i].style.fontWeight='bold';
- }
+// var li=document.getElementsByTagName('li');
+// li[1].style.backgroundColor='green';
+//  for(var i=0;i<li.length;i++)
+//  {
+//    li[i].style.fontWeight='bold';
+//  }
+
+var seconditem=document.querySelector('.list-group-item:nth-child(2)');
+seconditem.style.backgroundColor='green';
+
+var thirditem=document.querySelector('.list-group-item:nth-child(3)');
+thirditem.style.display='none';
