05b2112d60f2bbd4a36f91fa7dff014fa7aeed2c
VaibhavDhamanage <vaibhavdhamanage>
2023-11-09 12:04:10 +0530

    add new li element with same change

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-var items = document.getElementsByClassName('list-group-item');
-   console.log(items);
-   console.log(items[1]);
-   items[1].textContent="VAibhav";
-   items[1].style.backgroundColor='green';
-   items[1].style.fontWeight='bold';
-   items[2].style.fontWeight='bold';
-   items[3].style.fontWeight='bold';
-   items[0].style.fontWeight='bold';
+// var items = document.getElementsByClassName('list-group-item');
+//    console.log(items);
+//    console.log(items[1]);
+//    items[1].textContent="VAibhav";
+//    items[1].style.backgroundColor='green';
+//    items[1].style.fontWeight='bold';
+//    items[2].style.fontWeight='bold';
+//    items[3].style.fontWeight='bold';
+//    items[0].style.fontWeight='bold';
 
+var li=document.getElementsByTagName('li');
+li[1].style.backgroundColor='green';
+ for(var i=0;i<li.length;i++)
+ {
+   li[i].style.fontWeight='bold';
+ }
